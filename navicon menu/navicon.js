3b9c5c991b74c5bc760cdfb3b206ac56a8ff0ca0
Stuart <stu@alwaystwisted.com>
2013-02-21 16:31:05 +0000

    navicon menu pattern (1st pass)

---
+++ navicon menu/navicon.js
@@ -0,0 +1,11 @@
+var button = document.getElementById('navicon');
+
+button.onclick = function() {
+    var div = document.getElementById('mainnav');
+    if (div.style.display !== 'block') {
+        div.style.display = 'block';
+    }
+    else {
+        div.style.display = 'none';
+    }
+};
