b121fef5df0006e2f7b6b04effe39784556d422d
Stuart <stu@alwaystwisted.com>
2013-02-27 11:24:19 +0000

    add scroll to top pure JS solution

---
+++ scroll to top/scroll-to-top.js
@@ -0,0 +1,14 @@
+document.getElementById('scroll-to-top').onclick = function () {
+    scrollTo(document.body, 0, 300);
+}
+
+function scrollTo(element, to, duration) {
+    if (duration < 0) return;
+    var difference = to - element.scrollTop;
+    var perTick = difference / duration * 2;
+
+    setTimeout(function() {
+        element.scrollTop = element.scrollTop + perTick;
+        scrollTo(element, to, duration - 0);
+    }, 10);
+}
